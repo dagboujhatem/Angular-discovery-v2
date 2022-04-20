e91e1a4bcb1e8dcb80f65551b76f97b39639e617
dagbouj hatem <dagboujhatem@gmail.com>
2022-04-20 09:13:52 +0100

    séance 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { Page404Component } from './components/page404/page404.component';
+import { UpdateEmployeeComponent } from './components/update-employee/update-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'list',
+    pathMatch : 'full'
+  },
+  {
+    path:'list',
+    component: EmployeeListComponent
+  },
+  {
+    path:'create',
+    component: CreateEmployeeComponent
+  },
+  {
+    path:'update/:id',
+    component: UpdateEmployeeComponent
+  },
+  {
+    path:'**',
+    component: Page404Component
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './components/update-employee/update-employee.component';
+import { Page404Component } from './components/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    EmployeeListComponent,
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
