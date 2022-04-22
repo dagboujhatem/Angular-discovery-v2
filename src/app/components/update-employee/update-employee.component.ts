21cc15c797cf1d309a74a889bf9d89ff963d3653
dagbouj hatem <dagboujhatem@gmail.com>
2022-04-22 09:57:47 +0100

    update employee

--- src/app/components/update-employee/update-employee.component.ts
+++ src/app/components/update-employee/update-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-update-employee',
@@ -6,10 +9,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-employee.component.css']
 })
 export class UpdateEmployeeComponent implements OnInit {
-
-  constructor() { }
+  employeeForm?: FormGroup;
+  submitted = false;
+  id: any;
+  constructor(private employeeService:EmployeeService, private router: Router,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.employeeForm = new FormGroup({
+      firstName : new FormControl('', [Validators.required]),
+      lastName : new FormControl('', [Validators.required]),
+      email : new FormControl('', [Validators.required, Validators.email]),
+      password : new FormControl('', [Validators.required]),
+      age : new FormControl('', [Validators.required, Validators.min(18), Validators.max(100)]),
+      salary : new FormControl('', [Validators.required]),
+    });
+    this.getCurrentEmployee();
+  }
+
+  getCurrentEmployee(){
+    this.id = this.activatedRoute.snapshot.params['id'];
+    this.employeeService.getEmployeeById(this.id).subscribe((response:any)=>{
+      // console.log(response);
+      this.employeeForm?.patchValue(response);
+    }, (error:any)=>{
+      console.log(error);
+    })
+  }
+
+  updateEmployeeByID(){
+    this.submitted = true;
+    if(this.employeeForm?.invalid){
+      return;
+    }
+
+    // déclancher l'update
+    this.employeeService.updateEmployee(this.id, this.employeeForm?.value).subscribe((response:any)=>{
+      // console.log(response);
+      this.router.navigateByUrl('/list');
+    }, (error:any)=>{
+      console.log(error);
+    })
+    
   }
 
 }
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -21,4 +21,12 @@ export class EmployeeService {
   deleteEmployeeById(id:any){
     return this.http.delete(`${this.baseUrl}/employees/${id}`);
   }
+
+  getEmployeeById(id:any){
+    return this.http.get(`${this.baseUrl}/employees/${id}`);
+  }
+
+  updateEmployee(id:any, data: any){
+    return this.http.put(`${this.baseUrl}/employees/${id}`, data);
+  }
 }
